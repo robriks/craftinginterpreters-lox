40d143127243273ebd3eedddbfa88d280683b41d
robriks <marsterlund@gmail.com>
2024-04-24 18:04:06 -0400

    Add token and token type classes

---
+++ TokenType.java
@@ -0,0 +1,17 @@
+package com.craftinginterpreters.lox;
+
+enum TokenType {
+    // single char tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // one || two character tokens
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
